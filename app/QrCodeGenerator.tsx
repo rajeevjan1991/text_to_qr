bfd751e1eac4121db5bb1d3fc04200e932a1a630
rajeev <rajeevjan1991@gmail.com>
2025-01-10 11:24:48 +0530

    adding 8aiku name

--- app/QrCodeGenerator.tsx
+++ app/QrCodeGenerator.tsx
@@ -60,7 +60,7 @@ function QrCodeGenerator() {
       <Card className="flex-1 flex flex-col w-full h-auto mx-auto bg-[#ecf7ff]/80 backdrop-blur-md shadow-sm border-2 border-white/40 rounded-xl">
         <CardHeader>
           <CardTitle className="text-3xl font-bold text-center text-[#037fff]">
-            QR Code Generator
+            8aiku - QR Code Generator
           </CardTitle>
         </CardHeader>
         <CardContent className="flex-1">
